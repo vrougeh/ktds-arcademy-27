bc948f9bb0e2b0d6b3851ea0c87a6319473c1bc5
vrougeh <dlaghdrbs907@gmail.com>
2026-01-20 08:49:35 +0900

    fix : fix logic

--- java_exam/src/exam/PrimeNumber.java
+++ java_exam/src/exam/PrimeNumber.java
@@ -5,14 +5,14 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		// 2~100,000 사이 소수 출력		
 		for(int i = 2 ; i < 100_000 ; i++) {
-			int count = 0;
-			for(int j = 2 ; j < i ; j++) {
-				if(i % (double)j == 0) {
-					count++;
-					j = i;
+			boolean isPrime = true;
+			for(int j = 2 ; j*j < i ; j++) {
+				if(i % j == 0) {
+					isPrime = false;
+					break;
 				}
 			}
-			if(count == 0) {
+			if(isPrime) {
 			System.out.println(i);
 			}
 		}
