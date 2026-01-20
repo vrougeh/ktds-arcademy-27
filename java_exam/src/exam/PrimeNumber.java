0c1394e20f7e6ff939782b4263983ba2aac3a318
vrougeh <dlaghdrbs907@gmail.com>
2026-01-20 09:34:18 +0900

    fix : fix code

--- java_exam/src/exam/PrimeNumber.java
+++ java_exam/src/exam/PrimeNumber.java
@@ -4,8 +4,9 @@ public class PrimeNumber {
 
 	public static void main(String[] args) {
 		// 2~100,000 사이 소수 출력		
+		boolean isPrime = true;
 		for(int i = 2 ; i < 100_000 ; i++) {
-			boolean isPrime = true;
+			isPrime = true;
 			for(int j = 2 ; j*j < i ; j++) {
 				if(i % j == 0) {
 					isPrime = false;
@@ -16,5 +17,7 @@ public class PrimeNumber {
 			System.out.println(i);
 			}
 		}
+		
+		
 	}
 }
